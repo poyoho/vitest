013845fbfff817093dcf9f84baf82bf2892bd3f9
yoho <panyoho@gmail.com>
2022-12-03 21:21:14 +0800

    chore: update

--- test/core/test/url-import.test.ts
+++ test/core/test/url-import.test.ts
@@ -1,9 +1,7 @@
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 import { urls } from '../src/url-import'
 
-describe('url-import', () => {
-  it('builder', () => {
-    expect(urls().localWasm).toMatchInlineSnapshot('"/src/add.wasm"')
-    expect(urls().externalWasm).toMatchInlineSnapshot('"/@fs/jsmpeg-lite/jsmpeg.wasm"')
-  })
+it('wasm urls', () => {
+  expect(urls().localWasm).toMatchInlineSnapshot('"/src/add.wasm"')
+  expect(urls().externalWasm).toMatchInlineSnapshot('"/@fs/jsmpeg-lite/jsmpeg.wasm"')
 })
